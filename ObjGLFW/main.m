581177ec2a664ed89c4df746be365ad4cfe686fe
Yury Vovk <vovk.yury@me.com>
2017-12-07 21:32:11 +0300

    Test application

--- ObjGLFW/main.m
+++ ObjGLFW/main.m
@@ -7,11 +7,46 @@
 //
 
 #import <ObjFW/ObjFW.h>
+#import "GlfwGeometry.h"
+#import "GlfwApplication.h"
+#import "GlfwWindowManager.h"
+#import "GlfwEventHandling.h"
+#import "GlfwDrawing.h"
+#import "GlfwWindow.h"
+#import "GlfwEvent.h"
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        of_log(@"Hello, World!");
-    }
-    return 0;
+@interface AppDelegate: OFObject <OFApplicationDelegate, GlfwDrawing, GlfwEventHandling>
+
+@end
+
+GLFW_APPLICATION_DELEGATE(AppDelegate);
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching {
+    
+    GlfwWindow *newWindow = [[[GlfwWindow alloc] initWithRect:GlfwRectNew(0, 0, 640, 480) title:@"Test" hints:@{}] autorelease];
+    
+    [newWindow bindDrawble:self];
+    [newWindow bindEventHandler:self];
+    
+    [[GlfwWindowManager defaultManager] attachWindow:newWindow];
 }
+
+- (void)drawInWindow:(GlfwWindow *)window {
+    of_log(@"Drawing in %@", window);
+}
+
+- (GlfwEventMask)handledEventsMask {
+    return GlfwAnyEventMask;
+}
+
+- (void)handleEvent:(GlfwEvent *)event fromWindow:(GlfwWindow *)window {
+    of_log(@"Event %@ for window %@", event, window);
+}
+
+- (of_comparison_result_t)compare:(id<OFComparing>)object {
+    return OF_ORDERED_SAME;
+}
+
+@end
