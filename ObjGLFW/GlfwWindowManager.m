5cec2a449662ffdcea26030d367ec53f4d1b9178
GunGraveKoga <vovk.yury@me.com>
2017-12-07 03:24:55 +0300

    windows callbacks set/unset

--- ObjGLFW/GlfwWindowManager.m
+++ ObjGLFW/GlfwWindowManager.m
@@ -10,6 +10,8 @@
 #import "GlfwWindow.h"
 #import "GlfwApplication.h"
 
+#include "sokol_time.h"
+
 static void * _Nullable _windowRetain(void * _Nullable object) {
     return [(__bridge id)object copy];
 }
@@ -32,6 +34,22 @@ static of_map_table_functions_t _windowMapFunctions = {
     &_windowHash,
     &_windowIsEqual
 };
+
+static void windowPositionCallback(GLFWwindow *glfwWindow, int x, int y);
+static void windowSizeCallback(GLFWwindow *glfwWindow, int width, int height);
+static void windowCloseCallback(GLFWwindow *glfwWindow);
+static void windowRefreshCallback(GLFWwindow *glfwWindow);
+static void windowFocusCallback(GLFWwindow *glfwWindow, int hasFocus);
+static void windowIconifyCallback(GLFWwindow *glfwWindow, int toIconify);
+static void windowFramebufferSizeCallback(GLFWwindow *glfwWindow, int width, int height);
+static void inputMouseButtonCallback(GLFWwindow *glfwWindow, int button, int action, int mods);
+static void inputCursorPositionCallback(GLFWwindow *glfwWindow, double x, double y);
+static void inputCursorEnterCallback(GLFWwindow *glfwWindow, int enter);
+static void inputScrollCallback(GLFWwindow *glfwWindow, double xOffset, double yOffset);
+static void inputKeyCallback(GLFWwindow *glfwWindow, int key, int scancode, int action, int mods);
+static void inputCharCallback(GLFWwindow *glfwWindow, unsigned int codepoint);
+static void inputCharModCallback(GLFWwindow *glfwWindow, unsigned int codepoint, int mods);
+static void inputDropCallback(GLFWwindow *glfwWindow, int count, const char **paths);
 
 OF_INLINE bool onMainThread(void) {
     return ([OFThread currentThread] == [OFThread mainThread]);
@@ -69,6 +87,27 @@ OF_INLINE bool onMainThread(void) {
 #if defined(OF_HAVE_THREADS)
     if ( [_lock tryLock] ) {
 #endif
+      
+        GLFWwindow *handle = [window windowHandle];
+        glfwSetWindowUserPointer(handle, (void *)self);
+        
+        glfwSetWindowPosCallback(handle, &windowPositionCallback);
+        glfwSetWindowSizeCallback(handle, &windowSizeCallback);
+        glfwSetWindowCloseCallback(handle, &windowCloseCallback);
+        glfwSetWindowRefreshCallback(handle, &windowRefreshCallback);
+        glfwSetWindowFocusCallback(handle, &windowFocusCallback);
+        glfwSetWindowIconifyCallback(handle, &windowIconifyCallback);
+        glfwSetFramebufferSizeCallback(handle, &windowFramebufferSizeCallback);
+        glfwSetCharCallback(handle, &inputCharCallback);
+        glfwSetCharModsCallback(handle, &inputCharModCallback);
+        glfwSetCursorEnterCallback(handle, &inputCursorEnterCallback);
+        glfwSetCursorPosCallback(handle, &inputCursorPositionCallback);
+        glfwSetDropCallback(handle, &inputDropCallback);
+        glfwSetKeyCallback(handle, &inputKeyCallback);
+        glfwSetMouseButtonCallback(handle, &inputMouseButtonCallback);
+        glfwSetScrollCallback(handle, &inputScrollCallback);
+        
+        [_managedWindows setObject:(void *)window forKey:handle];
         
 #if defined(OF_HAVE_THREADS)
         [_lock unlock];
@@ -83,6 +122,28 @@ OF_INLINE bool onMainThread(void) {
 #if defined(OF_HAVE_THREADS)
     if ( [_lock tryLock] ) {
 #endif
+        
+        GLFWwindow *handle = [window windowHandle];
+        
+        glfwSetWindowPosCallback(handle, NULL);
+        glfwSetWindowSizeCallback(handle, NULL);
+        glfwSetWindowCloseCallback(handle, NULL);
+        glfwSetWindowRefreshCallback(handle, NULL);
+        glfwSetWindowFocusCallback(handle, NULL);
+        glfwSetWindowIconifyCallback(handle, NULL);
+        glfwSetFramebufferSizeCallback(handle, NULL);
+        glfwSetCharCallback(handle, NULL);
+        glfwSetCharModsCallback(handle, NULL);
+        glfwSetCursorEnterCallback(handle, NULL);
+        glfwSetCursorPosCallback(handle, NULL);
+        glfwSetDropCallback(handle, NULL);
+        glfwSetKeyCallback(handle, NULL);
+        glfwSetMouseButtonCallback(handle, NULL);
+        glfwSetScrollCallback(handle, NULL);
+        
+        glfwSetWindowUserPointer(handle, NULL);
+        
+        [_managedWindows removeObjectForKey:handle];
     
 #if defined(OF_HAVE_THREADS)
         [_lock unlock];
