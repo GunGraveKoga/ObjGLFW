d14470268e36e63973c1ba8c7384eb3a39623ebf
Yury Vovk <vovk.yury@me.com>
2017-12-07 20:04:41 +0300

    Event object implementation

--- ObjGLFW/GlfwEvent.h
+++ ObjGLFW/GlfwEvent.h
@@ -26,6 +26,17 @@ enum _GlfwEventType {
     GlfwMouseExited,
     GlfwKeyDown,
     GlfwKeyUp,
+    GlfwScrollWheel,
+    GlfwWindowMoved,
+    GlfwWindowResized,
+    GlfwWindowFramebuferResized,
+    GlfwWindowShouldRefresh,
+    GlfwWindowShouldClose,
+    GlfwWindowFocused,
+    GlfwWindowIconified,
+    GlfwCharacter,
+    GlfwModifiedCharacter,
+    GlfwFilesDrop,
 };
 typedef uint32_t GlfwEventType;
 
@@ -43,6 +54,17 @@ enum {
     GlfwMouseExitedMask = (1 << GlfwMouseExited),
     GlfwKeyDownMask = (1 << GlfwKeyDown),
     GlfwKeyUpMask = (1 << GlfwKeyUp),
+    GlfwScrollWheelMask = (1 << GlfwScrollWheel),
+    GlfwWindowMovedMask = (1 << GlfwWindowMoved),
+    GlfwWindowResizedMask = (1 << GlfwWindowResized),
+    GlfwWindowFramebuferResizedMask = (1 << GlfwWindowFramebuferResized),
+    GlfwWindowShouldRefreshMask = (1 << GlfwWindowShouldRefresh),
+    GlfwWindowShouldCloseMask = (1 << GlfwWindowShouldClose),
+    GlfwWindowFocusedMask = (1 << GlfwWindowFocused),
+    GlfwWindowIconifiedMask = (1 << GlfwWindowIconified),
+    GlfwCharacterMask = (1 << GlfwCharacter),
+    GlfwModifiedCharacterMask = (1 << GlfwModifiedCharacter),
+    GlfwFilesDropMask = (1 << GlfwFilesDrop),
     GlfwAnyEventMask = 0xffffffffU,
 };
 typedef unsigned long long GlfwEventMask;
@@ -53,16 +75,43 @@ OF_INLINE GlfwEventMask GlfwEventMaskFromType(GlfwEventType type) {
 
 @class GlfwRawWindow;
 
-@interface GlfwEvent : OFObject
+OF_ASSUME_NONNULL_BEGIN
+
+@interface GlfwEvent : OFObject <OFComparing>
 {
     double _timestamp;
-    
+    GlfwEventType _type;
+    of_point_t _mouseLocation;
+    GlfwRawWindow *_window;
+    int _modifierFlags;
+    of_unichar_t _character;
+    int _characterModifiers;
+    int _key;
+    int _scanCode;
+    int _mouseButton;
+    int _mouseModifiers;
+    double _deltaX, _deltaY;
+    GlfwSize _size;
+    GlfwPoint _pos;
+    OFArray OF_GENERIC(OFString *) *_paths;
 }
 
 @property (nonatomic, readonly) double timestamp;
 @property (nonatomic, readonly) GlfwEventType type;
 @property (nonatomic, readonly) of_point_t mouseLoaction;
 @property (nonatomic, readonly, retain) GlfwRawWindow *window;
+@property (nonatomic, readonly) int modifierFlags;
+@property (nonatomic, readonly) of_unichar_t character;
+@property (nonatomic, readonly) int characterModifiers;
+@property (nonatomic, readonly) int key;
+@property (nonatomic, readonly) int scanCode;
+@property (nonatomic, readonly) int mouseButton;
+@property (nonatomic, readonly) int mouseModifiers;
+@property (nonatomic, readonly) double deltaX;
+@property (nonatomic, readonly) double deltaY;
+@property (nonatomic, readonly) GlfwPoint pos;
+@property (nonatomic, readonly) GlfwSize size;
+@property (nonatomic, readonly, nullable, copy) OFArray OF_GENERIC(OFString *) *paths;
 
 + (instancetype)enterExitEventWithType:(GlfwEventType)type
                              timestamp:(double)timestamp
@@ -71,8 +120,33 @@ OF_INLINE GlfwEventMask GlfwEventMaskFromType(GlfwEventType type) {
 + (instancetype)keyEventWithType:(GlfwEventType)type
                        timestamp:(double)timestamp
                           window:(GlfwRawWindow *)window
+                             key:(int)key
                         scanCode:(int)scanCode
                        modifiers:(int)mods;
+
++ (instancetype)mouseEventWithType:(GlfwEventType)type
+                         timestamp:(double)timestamp
+                            window:(GlfwRawWindow *)window
+                          location:(of_point_t)location
+                            button:(int)mouseButton
+                         modifiers:(int)mouseButtonModifiers
+                            deltaX:(double)deltaX
+                            deltaY:(double)deltaY;
+
++ (instancetype)characterEventWithType:(GlfwEventType)type
+                             timestamp:(double)timestamp
+                                window:(GlfwRawWindow *)window
+                             character:(of_unichar_t)character
+           characterModifiers:(int)mods;
+
++ (instancetype)otherEventWithType:(GlfwEventType)type
+                         timestamp:(double)timestamp
+                            window:(GlfwRawWindow *)window
+                              size:(GlfwSize)size
+                               pos:(GlfwPoint)pos
+                             paths:(OFArray OF_GENERIC(OFString *) * _Nullable)paths;
+
+- (instancetype)init OF_UNAVAILABLE;
 
 - (instancetype)initWithType:(GlfwEventType)type
                     location:(of_point_t)location
@@ -80,11 +154,19 @@ OF_INLINE GlfwEventMask GlfwEventMaskFromType(GlfwEventType type) {
                    timestamp:(double)timestamp
                       window:(GlfwRawWindow *)window
                    character:(of_unichar_t)character
- charactersIgnoringModifiers:(int)mods
+          characterModifiers:(int)mods
+                         key:(int)key
                     scanCode:(int)scanCode
+                 mouseButton:(int)mouseButton
+              mouseModifiers:(int)mouseButtonModifiers
                       deltaX:(double)deltaX
-                      deltaY:(double)deltaY;
+                      deltaY:(double)deltaY
+                        size:(GlfwSize)size
+                        pos:(GlfwPoint)pos
+                       paths:(OFArray OF_GENERIC(OFString *) * _Nullable)paths OF_DESIGNATED_INITIALIZER;
 
-- (bool)isEventMatchEventMask:(GlfwEventMask)mask;
+- (bool)isMatchEventMask:(GlfwEventMask)mask;
 
 @end
+
+OF_ASSUME_NONNULL_END
--- ObjGLFW/GlfwEvent.m
+++ ObjGLFW/GlfwEvent.m
@@ -10,4 +10,84 @@
 
 @implementation GlfwEvent
 
+@synthesize timestamp = _timestamp, deltaX =_deltaX, deltaY = _deltaY;
+@synthesize type = _type;
+@synthesize mouseLoaction = _mouseLocation;
+@synthesize window = _window;
+@synthesize modifierFlags = _modifierFlags, scanCode = _scanCode, key = _key, characterModifiers = _characterModifiers, mouseButton = _mouseButton, mouseModifiers = _mouseModifiers;
+@synthesize paths = _paths;
+@synthesize size = _size;
+@synthesize pos = _pos;
+
++ (instancetype)mouseEventWithType:(GlfwEventType)type timestamp:(double)timestamp window:(GlfwRawWindow *)window location:(of_point_t)location button:(int)mouseButton modifiers:(int)mouseButtonModifiers deltaX:(double)deltaX deltaY:(double)deltaY
+{
+    
+    return [[[self alloc] initWithType:type location:location modifierFlags:0 timestamp:timestamp window:window character:0 characterModifiers:0 key:0 scanCode:0 mouseButton:mouseButton mouseModifiers:mouseButtonModifiers deltaX:deltaX deltaY:deltaY size:GlfwSizeZero() pos:GlfwPointZero() paths:nil] autorelease];
+}
+
++ (instancetype)keyEventWithType:(GlfwEventType)type timestamp:(double)timestamp window:(GlfwRawWindow *)window scanCode:(int)scanCode modifiers:(int)mods
+{
+    return [[[self alloc] initWithType:type location:of_point(0, 0) modifierFlags:mods timestamp:timestamp window:window character:0 characterModifiers:0 key:0 scanCode:scanCode mouseButton:0 mouseModifiers:0 deltaX:0 deltaY:0 size:GlfwSizeZero() pos:GlfwPointZero() paths:nil] autorelease];
+}
+
++ (instancetype)enterExitEventWithType:(GlfwEventType)type timestamp:(double)timestamp window:(GlfwRawWindow *)window
+{
+    return [[[self alloc] initWithType:type location:of_point(0, 0) modifierFlags:0 timestamp:timestamp window:window character:0 characterModifiers:0 key:0 scanCode:0 mouseButton:0 mouseModifiers:0 deltaX:0 deltaY:0 size:GlfwSizeZero() pos:GlfwPointZero() paths:nil] autorelease];
+}
+
++ (instancetype)characterEventWithType:(GlfwEventType)type timestamp:(double)timestamp window:(GlfwRawWindow *)window character:(of_unichar_t)character characterModifiers:(int)mods
+{
+    return [[[self alloc] initWithType:type location:of_point(0, 0) modifierFlags:0 timestamp:timestamp window:window character:character characterModifiers:mods key:0 scanCode:0 mouseButton:0 mouseModifiers:0 deltaX:0 deltaY:0 size:GlfwSizeZero() pos:GlfwPointZero() paths:nil] autorelease];
+}
+
++ (instancetype)otherEventWithType:(GlfwEventType)type timestamp:(double)timestamp window:(GlfwRawWindow *)window size:(GlfwSize)size pos:(GlfwPoint)pos paths:(OFArray<OFString *> *)paths
+{
+    return [[[self alloc] initWithType:type location:of_point(0, 0) modifierFlags:0 timestamp:timestamp window:window character:0 characterModifiers:0 key:0 scanCode:0 mouseButton:0 mouseModifiers:0 deltaX:0 deltaY:0 size:size pos:pos paths:paths] autorelease];
+}
+
+- (instancetype)initWithType:(GlfwEventType)type location:(of_point_t)location modifierFlags:(int)flags timestamp:(double)timestamp window:(GlfwRawWindow *)window character:(of_unichar_t)character characterModifiers:(int)mods key:(int)key scanCode:(int)scanCode mouseButton:(int)mouseButton mouseModifiers:(int)mouseButtonModifiers deltaX:(double)deltaX deltaY:(double)deltaY size:(GlfwSize)size pos:(GlfwPoint)pos paths:(OFArray<OFString *> *)paths
+{
+    self = [super init];
+    
+    _type = type;
+    _mouseLocation = location;
+    _modifierFlags = flags;
+    _timestamp = timestamp;
+    _window = [window retain];
+    _character = character;
+    _characterModifiers = mods;
+    _key = key;
+    _scanCode = scanCode;
+    _mouseButton = mouseButton;
+    _mouseModifiers = mouseButtonModifiers;
+    _deltaX = deltaY;
+    _deltaY = deltaY;
+    _size = size;
+    _pos = pos;
+    _paths = [paths copy];
+    
+    return self;
+}
+
+- (bool)isMatchEventMask:(GlfwEventMask)mask {
+    GlfwEventMask eventMask = GlfwEventMaskFromType(_type);
+    
+    return ((mask & eventMask) == eventMask);
+}
+
+- (of_comparison_result_t)compare:(id<OFComparing>)object {
+    if (![(id<OFObject>)object isMemberOfClass:[self class]])
+        @throw [OFInvalidArgumentException exception];
+    
+    GlfwEvent *other = (GlfwEvent *)object;
+    
+    if (self->_timestamp < other->_timestamp)
+        return OF_ORDERED_ASCENDING;
+    
+    if (self->_timestamp > other->_timestamp)
+        return OF_ORDERED_DESCENDING;
+    
+    return OF_ORDERED_SAME;
+}
+
 @end
