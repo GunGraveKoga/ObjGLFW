cb8b5a729e88db9d68ffd9d00c16fd953d17d07e
Yury Vovk <vovk.yury@me.com>
2017-12-08 19:54:12 +0300

    new geometry functions

--- ObjGLFW/GlfwGeometry.h
+++ ObjGLFW/GlfwGeometry.h
@@ -33,6 +33,14 @@ OF_INLINE GlfwPoint GlfwPointZero(void) {
     return GlfwPointNew(0, 0);
 }
 
+OF_INLINE GlfwPoint GlfwPointNull(void) {
+    return (GlfwPoint){NAN,NAN};
+}
+
+OF_INLINE bool GlfwPointIsNull(GlfwPoint point) {
+    return (isnan(point.x) || isnan(point.y));
+}
+
 OF_INLINE GlfwSize GlfwSizeNew(int width, int height) {
     GlfwSize size = {width, height};
     
@@ -41,6 +49,14 @@ OF_INLINE GlfwSize GlfwSizeNew(int width, int height) {
 
 OF_INLINE GlfwSize GlfwSizeZero(void) {
     return GlfwSizeNew(0, 0);
+}
+
+OF_INLINE GlfwSize GlfwSizeNull(void) {
+    return (GlfwSize){NAN, NAN};
+}
+
+OF_INLINE bool GlfwSizeIsNull(GlfwSize size) {
+    return (isnan(size.width) || isnan(size.height));
 }
 
 OF_INLINE GlfwRect GlfwRectNew(int x, int y, int width, int height) {
@@ -53,4 +69,44 @@ OF_INLINE GlfwRect GlfwRectNew(int x, int y, int width, int height) {
 
 OF_INLINE GlfwRect GlfwRectZero(void) {
     return GlfwRectNew(0, 0, 0, 0);
+}
+
+OF_INLINE GlfwRect GlfwNullRect(void) {
+    return (GlfwRect){{NAN,NAN},{NAN,NAN}};
+}
+
+OF_INLINE bool GlfwrectIsNull(GlfwRect rect) {
+    return (GlfwPointIsNull(rect.origin) || GlfwSizeIsNull(rect.size));
+}
+
+OF_INLINE of_point_t of_point_zero(void) {
+    return of_point(0, 0);
+}
+
+OF_INLINE of_point_t of_point_null(void) {
+    return (of_point_t){NAN,NAN};
+}
+
+OF_INLINE bool of_point_is_null(of_point_t point) {
+    return (isnan(point.x) || isnan(point.y));
+}
+
+OF_INLINE of_dimension_t of_dimension_zero(void) {
+    return of_dimension(0.0, 0.0);
+}
+
+OF_INLINE of_dimension_t of_dimension_null(void) {
+    return (of_dimension_t){NAN,NAN};
+}
+
+OF_INLINE bool of_dimension_is_null(of_dimension_t dimension) {
+    return (isnan(dimension.width) || isnan(dimension.width));
+}
+
+OF_INLINE of_rectangle_t of_rectangle_zero(void) {
+    return of_rectangle(0.0, 0.0, 0.0, 0.0);
+}
+
+OF_INLINE bool of_rectangle_is_null(of_rectangle_t rect) {
+    return (of_point_is_null(rect.origin) || of_dimension_is_null(rect.size));
 }
